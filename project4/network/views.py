54ed6c24621f0db2833a3af2121a7ec5817dcfba
vuca1/ <malihavita@gmail.com>
2026-08-04 14:06:26 +0200

    Add pagination to posts and user profiles, update templates accordingly

--- project4/network/views.py
+++ project4/network/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
@@ -18,11 +19,47 @@ class NewPostForm(forms.Form):
         })
     )
 
+
+POSTS_PER_PAGE = 10
+
+# TODO: adapt pagenation for every "post" page
 def index(request):
+    # divide to pages
+    p = Paginator(
+        Post.objects.all().order_by("-timestamp"),
+        POSTS_PER_PAGE
+    )
+
+    page_number = request.GET.get("page") if request.GET.get("page") else 1
+    page_obj = p.get_page(page_number) # get_page checks valid input
+
     return render(request, "network/index.html", {
         "new_post_form": NewPostForm(),
-        "posts": Post.objects.all().order_by("-timestamp")
+        "page_obj": page_obj,
     })
+
+
+def user_profile(request, user_id):
+    # get user by 'user_id'
+    user = get_object_or_404(User, id=user_id)
+    # check whether 'request.user' follows 'user_profile'
+    is_following = request.user.following.filter(pk=user.pk).exists()
+
+    # divide to pages
+    p = Paginator(
+        Post.objects.filter(author=user_id).order_by("-timestamp"),
+        POSTS_PER_PAGE
+    )
+    
+    page_number = request.GET.get("page") if request.GET.get("page") else 1
+    page_obj = p.get_page(page_number) # get_page checks valid input
+    
+    return render(request, "network/user.html", {
+        "user_profile": user,
+        "page_obj": page_obj,
+        "is_following": is_following  
+    })
+
 
 @login_required
 def create_post(request):
@@ -48,30 +85,23 @@ def create_post(request):
     return redirect("index")
 
 
-def user_profile(request, user_id):
-    user = get_object_or_404(User, id=user_id)
-    is_following = request.user.following.filter(pk=user.pk).exists()
-    
-    return render(request, "network/user.html", {
-        "user_profile": user,
-        "posts": Post.objects.filter(author=user_id).order_by("-timestamp"),
-        "is_following": is_following  
-    })
-
-
 @login_required
 def toggle_follow(request):
     if request.method == "POST":
+        # get 'user_id' from HTML form
         user_id = request.POST.get("user_id")
 
         if user_id is None:
             return redirect("index")
 
+        # get 'user' by 'user_id'
         user = get_object_or_404(User, pk=user_id)
 
+        # if logged in user is user to follow, redirect to 'index'
         if user == request.user:
             return redirect("index")
 
+        # follow or unfollow desired user
         if request.user.following.filter(pk=user.pk).exists():
             request.user.following.remove(user)
         else:
@@ -82,6 +112,7 @@ def toggle_follow(request):
 
 @login_required
 def following(request):
+    # render main page but only with posts from following users
     return render(request, "network/index.html", {
         "new_post_form": NewPostForm(),
         "posts": Post.objects
